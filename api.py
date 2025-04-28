45857c31de38891cec0e6d8a697ffef924f29de4
Eastforward <2499052783@qq.com>
2025-04-28 11:35:23 +0800

    remove auto download

--- api.py
+++ api.py
@@ -50,35 +50,16 @@ if __name__ == "__main__":
     model_type = "base"
     cnhubert_model_path = "pretrained_models/chinese-hubert-base"
     
-    from modelscope import snapshot_download
     try:
         if model_type == "base":
             model_path = "pretrained_models/Muyan-TTS"
-            snapshot_download('MYZY-AI/Muyan-TTS', local_dir=model_path)
         elif model_type == "sft":
             model_path = "pretrained_models/Muyan-TTS-SFT"
-            snapshot_download('MYZY-AI/Muyan-TTS-SFT', local_dir=model_path)
         else:
             print(f"Invalid model type: '{model_type}'. Please specify either 'base' or 'sft'.")
-        snapshot_download('pengzhendong/chinese-hubert-base', local_dir=cnhubert_model_path)
         print(f"Model downloaded successfully to {model_path}")
     except Exception as e:
         print(f"Error downloading model: {str(e)}")
-
-    # Or you can try to install from huggingface
-    # from huggingface_hub import snapshot_download
-    # try:
-    #     if model_type == "base":
-    #         snapshot_download('MYZY-AI/Muyan-TTS', local_dir=model_path)
-    #     elif model_type == "sft":
-    #         model_path = "pretrained_models/Muyan-TTS-SFT"
-    #         snapshot_download('MYZY-AI/Muyan-TTS-SFT', local_dir=model_path)
-    #     else:
-    #         print(f"Invalid model type: '{model_type}'. Please specify either 'base' or 'sft'.")
-    #     snapshot_download('TencentGameMate/chinese-hubert-base', local_dir=cnhubert_model_path)
-    #     print(f"Model downloaded successfully to {model_path}")
-    # except Exception as e:
-    #     print(f"Error downloading model: {str(e)}")
     
     tts = Inference(model_type, model_path, enable_vllm_acc=True)
     uvicorn.run(app, host="0.0.0.0", port=TTS_PORT)
